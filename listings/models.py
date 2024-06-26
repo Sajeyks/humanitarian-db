79c457dffb1f481ee2982f47a4e4fda4e844a9f2
Sajeyks <sajeyksmwangi@gmail.com>
2024-06-26 11:54:15 +0300

    feat: Add models for FallenSoldier, ArrestedPerson, and MissingPerson
    The code changes add models for FallenSoldier, ArrestedPerson, and MissingPerson to the listings app. These models include fields for various attributes such as name, reason of death, place killed, location, date, police station, has representation, time of missing, found, and date found.

---
+++ listings/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+class FallenSoldier(models.Model):
+    name = models.CharField(max_length=100)
+    reason_of_death = models.TextField()
+    place_killed = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    date = models.DateField()
+
+class ArrestedPerson(models.Model):
+    name = models.CharField(max_length=100)
+    police_station = models.CharField(max_length=100)
+    has_representation = models.BooleanField(default=False)
+    date = models.DateField()
+
+class MissingPerson(models.Model):
+    name = models.CharField(max_length=100)
+    time_of_missing = models.DateTimeField()
+    found = models.BooleanField(default=False)
+    date_found = models.DateField(null=True, blank=True)
