7a30b2b2ecefb2bc937b2f377bb435e29af616e3
Sajeyks <sajeyksmwangi@gmail.com>
2024-06-27 01:00:44 +0300

    refactor: Update admin site registration for models
    The code changes update the admin site registration for the `FallenSoldier`, `ArrestedPerson`, and `MissingPerson` models. Instead of directly registering the models, separate admin classes are created (`FallenSoldierAdmin`, `ArrestedPersonAdmin`, `MissingPersonAdmin`) with customized `list_display` attributes. These changes improve the organization and customization of the admin interface for managing the records.

--- listings/admin.py
+++ listings/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
 from .models import FallenSoldier, ArrestedPerson, MissingPerson
 
-admin.site.register(FallenSoldier)
-admin.site.register(ArrestedPerson)
-admin.site.register(MissingPerson)
+class FallenSoldierAdmin(admin.ModelAdmin):
+    list_display = ('name', 'photo')
+
+class ArrestedPersonAdmin(admin.ModelAdmin):
+    list_display = ('name', 'photo')
+
+class MissingPersonAdmin(admin.ModelAdmin):
+    list_display = ('name', 'photo')
+
+admin.site.register(FallenSoldier, FallenSoldierAdmin)
+admin.site.register(ArrestedPerson, ArrestedPersonAdmin)
+admin.site.register(MissingPerson, MissingPersonAdmin)
