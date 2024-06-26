24ca74bfd865f99f391062cad5f6fff233c97ac4
Sajeyks <sajeyksmwangi@gmail.com>
2024-06-27 00:28:10 +0300

    refactor: Update photo upload paths for models

--- listings/views.py
+++ listings/views.py
@@ -3,8 +3,9 @@ from .models import FallenSoldier, ArrestedPerson, MissingPerson
 
 def home(request):
     fallen_soldiers = FallenSoldier.objects.all().order_by('-date')
-    arrested_people = ArrestedPerson.objects.all().order_by('-date')
-    missing_people = MissingPerson.objects.all().order_by('-time_of_missing')
+    arrested_people = ArrestedPerson.objects.filter(has_representation=False).order_by('-date')
+    # missing_people = MissingPerson.objects.filter(found=False).order_by('-time_of_missing')
+    missing_people = MissingPerson.objects.order_by('-time_of_missing')
     
     context = {
         'fallen_soldiers': fallen_soldiers,
