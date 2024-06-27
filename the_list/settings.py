020bd7facf57c332e6634a1a3e9121ac9a416277
Sajeyks <sajeyksmwangi@gmail.com>
2024-06-27 15:45:51 +0300

    refactor: Update database configuration in settings.py
    Update database configuration in settings.py to use PostgreSQL and read database credentials from environment variables. Improves security and flexibility of the application's database setup.

--- the_list/settings.py
+++ the_list/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'listings',
+    'django_cleanup.apps.CleanupConfig',
 ]
 
 MIDDLEWARE = [
@@ -78,18 +79,25 @@ WSGI_APPLICATION = 'the_list.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': os.environ['DB_NAME'],
+#         'USER': os.environ['DB_USER'],
+#         'PASSWORD': os.environ['DB_PASSWORD'],
+#         'HOST': os.environ['DB_HOST'],
+#         'PORT': os.environ['DB_PORT'],
+#         'ATOMIC_REQUESTS': True,
+#         'CONN_MAX_AGE': 0,
+#         'CONN_HEALTH_CHECKS': True,
+#         'TIME_ZONE' : 'UTC',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ['DB_NAME'],
-        'USER': os.environ['DB_USER'],
-        'PASSWORD': os.environ['DB_PASSWORD'],
-        'HOST': os.environ['DB_HOST'],
-        'PORT': os.environ['DB_PORT'],
-        'ATOMIC_REQUESTS': True,
-        'CONN_MAX_AGE': 0,
-        'CONN_HEALTH_CHECKS': True,
-        'TIME_ZONE' : 'UTC',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
