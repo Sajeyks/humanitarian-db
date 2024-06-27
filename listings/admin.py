0df175b5650ba03ba3751891e08d927ad81ddc3c
Sajeyks <sajeyksmwangi@gmail.com>
2024-06-27 15:21:31 +0300

    refactor: Update admin site registration for models

--- listings/admin.py
+++ listings/admin.py
@@ -2,14 +2,17 @@ from django.contrib import admin
 from .models import FallenSoldier, ArrestedPerson, MissingPerson
 
 class FallenSoldierAdmin(admin.ModelAdmin):
-    list_display = ('name', 'photo')
+    list_display = ('name', 'image_tag')
+    readonly_fields = ['image_tag']
 
 class ArrestedPersonAdmin(admin.ModelAdmin):
-    list_display = ('name', 'photo')
+    list_display = ('name', 'image_tag')
+    readonly_fields = ['image_tag']
 
 class MissingPersonAdmin(admin.ModelAdmin):
-    list_display = ('name', 'photo')
+    list_display = ('name', 'image_tag')
+    readonly_fields = ['image_tag']
 
 admin.site.register(FallenSoldier, FallenSoldierAdmin)
 admin.site.register(ArrestedPerson, ArrestedPersonAdmin)
-admin.site.register(MissingPerson, MissingPersonAdmin)
+admin.site.register(MissingPerson, MissingPersonAdmin)#4a8acf
