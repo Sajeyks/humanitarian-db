298d71149bee30f6fe331d59121334e592050229
Sajeyks <sajeyksmwangi@gmail.com>
2024-07-14 17:22:57 +0300

    refactor: Remove commented out code and unused admin classes
    The code changes in the `listings/admin.py` file remove commented out code and unused admin classes (`FallenSoldierAdmin` and `ArrestedPersonAdmin`). This refactor improves code cleanliness and removes unnecessary clutter from the file.

--- listings/admin.py
+++ listings/admin.py
@@ -1,18 +1,19 @@
 from django.contrib import admin
-from .models import FallenSoldier, ArrestedPerson, MissingPerson
+from .models import MissingPerson #, FallenSoldier, ArrestedPerson,
 
-class FallenSoldierAdmin(admin.ModelAdmin):
-    list_display = ('name', 'image_tag')
-    readonly_fields = ['image_tag']
+# class FallenSoldierAdmin(admin.ModelAdmin):
+#     list_display = ('name', 'image_tag')
+#     readonly_fields = ['image_tag']
 
-class ArrestedPersonAdmin(admin.ModelAdmin):
-    list_display = ('name', 'image_tag')
-    readonly_fields = ['image_tag']
+# class ArrestedPersonAdmin(admin.ModelAdmin):
+#     list_display = ('name', 'image_tag')
+#     readonly_fields = ['image_tag']
 
 class MissingPersonAdmin(admin.ModelAdmin):
-    list_display = ('name', 'image_tag')
-    readonly_fields = ['image_tag']
+    # list_display = ('name', 'image_tag')
+    # readonly_fields = ['image_tag']
+    pass
 
-admin.site.register(FallenSoldier, FallenSoldierAdmin)
-admin.site.register(ArrestedPerson, ArrestedPersonAdmin)
+# admin.site.register(FallenSoldier, FallenSoldierAdmin)
+# admin.site.register(ArrestedPerson, ArrestedPersonAdmin)
 admin.site.register(MissingPerson, MissingPersonAdmin)#4a8acf
