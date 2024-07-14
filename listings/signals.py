3415f40af4576fe0fcf02519608fd800ac142843
Sajeyks <sajeyksmwangi@gmail.com>
2024-07-14 19:21:36 +0300

    refactor: Update cloudinary configuration and storage settings

--- listings/signals.py
+++ listings/signals.py
@@ -1,12 +1,12 @@
 from django.db.models.signals import post_delete, pre_save
 from django.dispatch import receiver
-from cloudinary.uploader import destroy
+from cloudinary import uploader
 from .models import MissingPerson
 
 @receiver(post_delete, sender=MissingPerson)
 def delete_image_on_delete(sender, instance, **kwargs):
-    if instance.image:
-        destroy(instance.image.public_id)
+    if instance.photo:
+        uploader.destroy(instance.photo.name)
 
 @receiver(pre_save, sender=MissingPerson)
 def delete_old_image_on_update(sender, instance, **kwargs):
@@ -18,5 +18,5 @@ def delete_old_image_on_update(sender, instance, **kwargs):
     except MissingPerson.DoesNotExist:
         return False
 
-    if old_instance.image and old_instance.image != instance.image:
-        destroy(old_instance.image.public_id)
+    if old_instance.photo and old_instance.photo != instance.photo:
+        uploader.destroy(old_instance.photo.name)
