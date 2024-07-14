cd94f4a9ef6f98f6e2c755e683c03380d00602d7
Sajeyks <sajeyksmwangi@gmail.com>
2024-07-14 17:25:16 +0300

    refactor: Remove unused migration files

--- listings/views.py
+++ listings/views.py
@@ -1,15 +1,15 @@
 from django.shortcuts import render
-from .models import FallenSoldier, ArrestedPerson, MissingPerson
+from .models import MissingPerson #, FallenSoldier, ArrestedPerson,
 
 def home(request):
-    fallen_soldiers = FallenSoldier.objects.all().order_by('-date')
-    arrested_people = ArrestedPerson.objects.filter(has_representation=False).order_by('-date')
+    # fallen_soldiers = FallenSoldier.objects.all().order_by('-date')
+    # arrested_people = ArrestedPerson.objects.filter(has_representation=False).order_by('-date')
     # missing_people = MissingPerson.objects.filter(found=False).order_by('-time_of_missing')
     missing_people = MissingPerson.objects.order_by('-time_of_missing')
     
     context = {
-        'fallen_soldiers': fallen_soldiers,
-        'arrested_people': arrested_people,
+        # 'fallen_soldiers': fallen_soldiers,
+        # 'arrested_people': arrested_people,
         'missing_people': missing_people,
     }
     return render(request, 'listings/home.html', context)
