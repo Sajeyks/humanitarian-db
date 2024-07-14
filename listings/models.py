123379a6fd73d0dd52111f945bedf36e3aa32e67
Sajeyks <sajeyksmwangi@gmail.com>
2024-07-14 17:22:45 +0300

    refactor: Update image upload paths for models

--- listings/models.py
+++ listings/models.py
@@ -1,43 +1,48 @@
 from django.db import models
 from django.utils.safestring import mark_safe
+from cloudinary.models import CloudinaryField
 
-class FallenSoldier(models.Model):
-    name = models.CharField(max_length=100)
-    reason_of_death = models.TextField()
-    place_killed = models.CharField(max_length=100)
-    location = models.CharField(max_length=100)
-    date = models.DateField()
-    photo = models.ImageField(upload_to='fallen_soldiers/', null=True, blank=True)
+# class FallenSoldier(models.Model):
+#     name = models.CharField(max_length=100)
+#     reason_of_death = models.TextField()
+#     place_killed = models.CharField(max_length=100)
+#     location = models.CharField(max_length=100)
+#     date = models.DateField()
+#     photo = models.ImageField(upload_to='fallen_soldiers/', null=True, blank=True)
     
-    def image_tag(self):
-        return mark_safe('<img src="/media/%s" width="150" height="150" />' % (self.photo))
+#     def image_tag(self):
+#         return mark_safe('<img src="/media/%s" width="150" height="150" />' % (self.photo))
 
-    def __str__(self):
-        return self.name
+#     def __str__(self):
+#         return self.name
 
-class ArrestedPerson(models.Model):
-    name = models.CharField(max_length=100)
-    police_station = models.CharField(max_length=100)
-    has_representation = models.BooleanField(default=False)
-    date = models.DateField()
-    photo = models.ImageField(upload_to='media/arrested_people/', null=True, blank=True)
+# class ArrestedPerson(models.Model):
+#     name = models.CharField(max_length=100)
+#     police_station = models.CharField(max_length=100)
+#     has_representation = models.BooleanField(default=False)
+#     date = models.DateField()
+#     photo = models.ImageField(upload_to='media/arrested_people/', null=True, blank=True)
     
-    def image_tag(self):
-        return mark_safe('<img src="/media/%s" width="150" height="150" />' % (self.photo))
+#     def image_tag(self):
+#         return mark_safe('<img src="/media/%s" width="150" height="150" />' % (self.photo))
 
 
-    def __str__(self):
-        return self.name
+#     def __str__(self):
+#         return self.name
 
 class MissingPerson(models.Model):
     name = models.CharField(max_length=100)
+    contact = models.CharField(max_length=15, unique=True)
     time_of_missing = models.DateTimeField()
     found = models.BooleanField(default=False)
     date_found = models.DateField(null=True, blank=True)
-    photo = models.ImageField(upload_to='missing_people/', null=True, blank=True)
+    
+    photo = CloudinaryField('image', transformation={
+        'width': 400, 'height': 300, 'crop': 'limit', 'quality': 'auto:good'
+    })
 
-    def image_tag(self):
-        return mark_safe('<img src="/media/%s" width="150" height="150" />' % (self.photo))
+    # def image_tag(self):
+    #     return mark_safe('<img src="/media/%s" width="150" height="150" />' % (self.photo))
 
     def __str__(self):
         return self.name
